99b606007af1a490150e20a04b5fcc3e9f0bc8dd
SwainWong <swain.wong@sunqtech.com>
2018-07-19 23:07:02 +0800

    stable version for android

--- Douban/App.js
+++ Douban/App.js
@@ -6,14 +6,14 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 
-import {TabBarNavigator} from './src/Router';
+import { TabBarNavigator } from './src/Router';
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
-    return <TabBarNavigator/>;
+    return <TabBarNavigator />;
   }
 }
--- Douban/src/Router.js
+++ Douban/src/Router.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import {  
+import {
     Image,
 } from 'react-native';
 
-import { 
+import {
     createBottomTabNavigator,
     createStackNavigator,
- } from "react-navigation";
+} from "react-navigation";
 
 import DBBookView from "./features/book/DBBookView";
 import DBMovieView from './features/movie/DBMovieView';
@@ -16,25 +16,25 @@ import DBLocationView from './features/location/DBLocationView';
 // 书
 const BookStackNavigator = createStackNavigator({
     DBBookView: DBBookView,
-    
+
 });
 
 // 电影
 const MovieStackNavigator = createStackNavigator({
     DBMovieView: DBMovieView,
-    
+
 });
 
 // 音乐
 const MusicStackNavigator = createStackNavigator({
     DBMusicView: DBMusicView,
-    
+
 });
 
 // 同城
 const LocationStackNavigator = createStackNavigator({
     DBLocationView: DBLocationView,
-    
+
 });
 
 const TabBarNavigator = createBottomTabNavigator(
@@ -43,23 +43,23 @@ const TabBarNavigator = createBottomTabNavigator(
             screen: BookStackNavigator,
             navigationOptions: {
                 tabBarLabel: "图书",
-                tabBarIcon: ({focused, tintColor}) => (
+                tabBarIcon: ({ focused, tintColor }) => (
 
                     <Image source={focused ? require('./assets/tabbar/db_book_selected.png') :
-                                                require('./assets/tabbar/db_book_normal.png')}/>
+                        require('./assets/tabbar/db_book_normal.png')} />
 
                 )
-                
+
             },
         },
-        
+
         Movie: {
             screen: MovieStackNavigator,
             navigationOptions: {
                 tabBarLabel: "电影",
-                tabBarIcon: ({focused, tintColor}) => (
+                tabBarIcon: ({ focused, tintColor }) => (
                     <Image source={focused ? require('./assets/tabbar/db_movie_selected.png') :
-                                                require('./assets/tabbar/db_movie_normal.png')}/>
+                        require('./assets/tabbar/db_movie_normal.png')} />
                 )
             },
         },
@@ -67,9 +67,9 @@ const TabBarNavigator = createBottomTabNavigator(
             screen: MusicStackNavigator,
             navigationOptions: {
                 tabBarLabel: "音乐",
-                tabBarIcon: ({focused, tintColor}) => (
-                    <Image  source={focused ? require('./assets/tabbar/db_music_selected.png') : 
-                                                require('./assets/tabbar/db_music_normal.png')}/>
+                tabBarIcon: ({ focused, tintColor }) => (
+                    <Image source={focused ? require('./assets/tabbar/db_music_selected.png') :
+                        require('./assets/tabbar/db_music_normal.png')} />
                 )
             },
         },
@@ -77,20 +77,20 @@ const TabBarNavigator = createBottomTabNavigator(
             screen: LocationStackNavigator,
             navigationOptions: {
                 tabBarLabel: "同城",
-                tabBarIcon: ({focused, tintColor}) => (
-                    <Image source={focused ? require('./assets/tabbar/db_location_selected.png') : 
-                                                require('./assets/tabbar/db_location_normal.png')}/>
+                tabBarIcon: ({ focused, tintColor }) => (
+                    <Image source={focused ? require('./assets/tabbar/db_location_selected.png') :
+                        require('./assets/tabbar/db_location_normal.png')} />
                 )
             },
         },
     },
     {
-        
+
         tabBarOptions: {
             inactiveTintColor: '#515151',
             activeTintColor: '#4c8dae',
             labelStyle: {
-              fontSize: 12,
+                fontSize: 12,
             },
         }
     }
